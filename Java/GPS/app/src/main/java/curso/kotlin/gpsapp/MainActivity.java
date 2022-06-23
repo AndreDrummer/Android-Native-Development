19585ddfdd85014db4ac489b81e1fab2fdf032d6
Andre Silva <academicworks.adf@gmail.com>
2022-06-23 08:30:03 -0300

    creating layout of app GPS JAVA

---
+++ Java/GPS/app/src/main/java/curso/kotlin/gpsapp/MainActivity.java
@@ -0,0 +1,14 @@
+package curso.kotlin.gpsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
