bf22c97b53d97fb15df6e9b8ed2e0b1c9b81b999
Andre Silva <academicworks.adf@gmail.com>
2022-10-25 18:24:53 -0300

    working with state compose

---
+++ Doc/Kotlin/BasicStateCodelab/app/src/main/java/curso/kotlin/basicstatecodelab/WelnessScreen.kt
@@ -0,0 +1,10 @@
+package curso.kotlin.basicstatecodelab
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    StatefulCounter(modifier)
+}
